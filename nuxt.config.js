45b7969ed975a7b4263424567c89b661fa0146c9
AykutSarac <aykutsarac0@gmail.com>
2020-12-31 15:11:27 +0300

    reuploading demo

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/github-gram/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
